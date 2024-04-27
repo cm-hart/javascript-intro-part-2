3c806b09fc1038d2bbc32ee98295130b6631285a
catiecodes <catiehart#mac.com>
2024-04-27 09:55:44 -0700

    added comments to js for exercises - draft one

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,37 @@
-//store the first name in a variable
+// 1. Log a string to the console
+// 2. Log a number to the console
+// 3. Log some simple addition to the console - that's right, JavaScript can do math!
+// 4. Create a variable called num and assign it a value of 1
+//      Log the variable to the console
+// 5. Reassign the value of the variable num to 2
+//      Log the variable to the console
+// 6. Create a new variable called plusOne and assign it the value of the first variable    plus 1
+//      Log the new variable to the console
+// 7. How do we keep adding plus one more?
+        //They will likely want to create another variable and this will eventually create a problem
+        //How can we solve this?
+        // num = num + 1;
 
-//store the count number in a variable
 
-//store the date in a variable
+let num = 1;
+let plusOne = num + 1;
+
 
 // OTHER ACTIVITIES:
+
+
+
+
+
+
+
+
+//What was done yesterday:
+//  store the first name in a variable
+//  store the count number in a variable
+//  store the date in a variable
+
+// OTHER ACTIVITIES FROM YESTERDAY:
 // 	Identify all dynamic content on a page
 //	Store the content in variables (naming conventions?)
 
-console.log('Hello World!');
