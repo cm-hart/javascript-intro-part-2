fa82a16190331e0c886140dd7dd6ee2c8bef0449
catiecodes <catiehart#mac.com>
2024-04-27 08:36:22 -0700

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+//store the first name in a variable
+
+//store the count number in a variable
+
+//store the date in a variable
+
+// OTHER ACTIVITIES:
+// 	Identify all dynamic content on a page
+//	Store the content in variables (naming conventions?)
+
+console.log('Hello World!');
